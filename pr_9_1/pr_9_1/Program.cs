aa444c1dbd44c8b8c4e474748610bc538239e810
123rjwq <145851361+123rjwq@users.noreply.github.com>
2023-12-18 04:21:28 +0300

    Add files via upload

---
+++ pr_9_1/pr_9_1/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+// Интерфейс для управления транспортным средством
+interface IVehicle
+{
+    void ChangeGear(int newGear);
+    // Метод смены передачи
+    void Accelerate(int speed);
+    // Метод ускорения
+    void Decelerate(int speed);
+    // Метод замедления
+    void Refuel(string fuelType);
+    // Метод заправки топливом
+    void Service();
+    // Метод технического обслуживания
+    void TurnLeft();
+    // Метод поворота налево
+    void TurnRight();
+    // Метод поворота направо
+    void ShowInfo();
+    // Метод вывода информации
+}
+
+// Базовый класс для транспортных средств
+abstract class Transport : IVehicle
+{
+    protected int speed;
+    // Скорость транспорта
+    protected int gear;
+    // Текущая передача
+
+    public void ChangeGear(int newGear)
+    {
+        gear = newGear;
+        Console.WriteLine($"Переключена передача на {gear}.");
+    }
+
+    public void Accelerate(int increaseSpeed)
+    {
+        speed += increaseSpeed;
+        Console.WriteLine($"Ускорение до {speed} км/ч.");
+    }
+
+    public void Decelerate(int decreaseSpeed)
+    {
+        speed -= decreaseSpeed;
+        Console.WriteLine($"Замедление до {speed} км/ч.");
+    }
+
+    public abstract void Refuel(string fuelType);
+
+    public abstract void Service();
+
+    public abstract void TurnLeft();
+
+    public abstract void TurnRight();
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Текущая скорость: {speed} км/ч, Текущая передача: {gear}");
+    }
+}
+
+// Класс для легкового автомобиля с бензиновым двигателем
+class GasolineCar : Transport
+{
+    public override void Refuel(string fuelType)
+    {
+        if (fuelType == "бензин")
+            Console.WriteLine("Заправлено бензином.");
+        else
+            Console.WriteLine("Неверный тип топлива для бензинового автомобиля.");
+    }
+
+    public override void Service()
+    {
+        Console.WriteLine("Выполнено техническое обслуживание для бензинового автомобиля");
+    }
+
+    public override void TurnLeft()
+    {
+        Console.WriteLine("Поворот налево поворотом передних колес.");
+    }
+
+    public override void TurnRight()
+    {
+        Console.WriteLine("Поворот направо поворотом передних колес.");
+    }
+}
+
+// Класс для легкового автомобиля с дизельным двигателем
+class DieselCar : Transport
+{
+    public override void Refuel(string fuelType)
+    {
+        if (fuelType == "дизель")
+            Console.WriteLine("Заправлено дизельным топливом.");
+        else
+            Console.WriteLine("Неверный тип топлива для дизельного автомобиля.");
+    }
+
+    public override void Service()
+    {
+        Console.WriteLine("Выполнено техническое обслуживание для дизельного автомобиля. Поменяно масло и фильтры в двигателе");
+    }
+
+    public override void TurnLeft()
+    {
+        Console.WriteLine("Поворот налево поворотом передних колес.");
+    }
+
+    public override void TurnRight()
+    {
+        Console.WriteLine("Поворот направо поворотом передних колес.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        GasolineCar gasolineCar = new GasolineCar();
+        // Создание объекта бензиновый автомобиль
+        DieselCar dieselCar = new DieselCar();
+        // Создание объекта дизельный автомобиль
+
+        // Пример использования методов
+        gasolineCar.ChangeGear(2);
+        // Смена передачи
+        gasolineCar.Accelerate(50);
+        // Ускорение
+        gasolineCar.TurnLeft();
+        // Поворот налево
+        gasolineCar.Refuel("бензин");
+        // Заправка бензином
+        gasolineCar.Service();
+        // Техническое обслуживание
+        gasolineCar.ShowInfo();
+        // Вывод информации
+
+        dieselCar.ChangeGear(1);
+        // Смена передачи
+        dieselCar.Decelerate(10);
+        // Замедление
+        dieselCar.TurnRight();
+        // Поворот направо
+        dieselCar.Refuel("дизель");
+        // Заправка дизельным топливом
+        dieselCar.Service();
+        // Техническое обслуживание
+        dieselCar.ShowInfo();
+        // Вывод информации
+    }
+}
---
+++ pr_9_2/pr_9_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pr_9_2
+{
+    class Program
+    {
+
+        // Интерфейс для движения
+        interface IMovable
+        {
+            const int MinimumSpeed = 0;
+            // Минимальная скорость
+            static int MaximumSpeed = 60;
+            // Максимальная скорость
+
+            void Move();
+            // Метод движения
+
+            string Name { get; set; }
+            // Свойство имени
+
+            delegate void MoveHandler(string message);
+            // Определение делегата для события
+
+            event MoveHandler MoveEvent;
+            // Событие движения
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IMovable.MaximumSpeed);
+            // Вывод максимальной скорости
+            Console.WriteLine(IMovable.MinimumSpeed);
+            // Вывод минимальной скорости
+        }
+    }
+}
